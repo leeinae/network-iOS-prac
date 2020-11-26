43288d8b65eefe100840d6b9ade62abda605f411
inaeLee <inae9709@hanmail.net>
2020-11-26 15:36:35 +0900

    setting: foldering add: tableView 레이아웃 잡기

---
+++ network-iOS-prac/Source/Cell/TableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TableViewCell.swift
+//  network-iOS-prac
+//
+//  Created by inae Lee on 2020/11/26.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+    static let identifier = "TableViewCell"
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
