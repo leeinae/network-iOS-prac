1a786300d17db966434c8ca60778f91ea2c4ebb3
inaeLee <inae9709@hanmail.net>
2020-11-26 15:21:30 +0900

    init commit

---
+++ network-iOS-prac/Source/VC/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  network-iOS-prac
+//
+//  Created by inae Lee on 2020/11/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
